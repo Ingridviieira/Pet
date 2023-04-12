5d3e09b32457a3b584260ffa24c80efc97e06546
Ingridviieira <rm95396@fiap.com.br>
2023-04-12 11:56:15 -0300

    yessss

--- PetMania/PetMania/src/main/java/br/com/fiap/PetMania/repository/AnimalRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.PetMania.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.PetMania.models.Animal;
-
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
-    
-}
