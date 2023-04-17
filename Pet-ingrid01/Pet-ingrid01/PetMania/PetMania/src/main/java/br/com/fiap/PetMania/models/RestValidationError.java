fe14a654d71eb485eaf310bee055e4e004d03f51
Ingridviieria <rm95396@fiap.com.br>
2023-04-17 12:31:42 -0300

    Projeto atualizado validação feita

--- Pet-ingrid01/Pet-ingrid01/PetMania/PetMania/src/main/java/br/com/fiap/PetMania/models/RestValidationError.java
+++
@@ -1,3 +0,0 @@
-package br.com.fiap.PetMania.models;
-
-public record RestValidationError(String field, String message) {} 
--- Pet-ingrid01/Pet-ingrid01/PetMania/PetMania/src/main/java/br/com/fiap/PetMania/repository/GastosRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.PetMania.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.PetMania.models.GastosAnimal;
-
-public interface GastosRepository extends JpaRepository<GastosAnimal, Long>{
-
-}
